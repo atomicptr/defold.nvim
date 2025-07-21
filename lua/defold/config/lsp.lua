a9364c7a2fde8d01767cf00e88bffca8bfa829a8
Christopher Kaster <me@atomicptr.de>
2025-07-21 16:45:04 +0200

    remove redundant 'lua' path

--- lua/defold/config/lsp.lua
+++ lua/defold/config/lsp.lua
@@ -1,7 +1,7 @@
 local defold = require "defold"
 local project = require "defold.project"
 
-local libs = { "lua", defold.defold_api_path() }
+local libs = { defold.defold_api_path() }
 
 for _, lib in ipairs(project.dependency_api_paths()) do
     table.insert(libs, lib)
