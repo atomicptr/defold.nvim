78820470c2fdc5a06e7765157174ff2f382358af
Christopher Kaster <me@atomicptr.de>
2026-04-07 17:10:10 +0200

    move pak update event to plugin/defold.lua

--- plugin/defold.lua
+++ plugin/defold.lua
@@ -1,1 +1,15 @@
-require("defold").setup()
+require("defold").setup {}
+
+if vim.version.cmp(vim.version(), { 0, 12, 0 }) >= 0 then
+    vim.api.nvim_create_autocmd("PackChanged", {
+        callback = function(ev)
+            if ev.data.spec.name ~= "defold.nvim" then
+                return
+            end
+
+            if ev.data.kind == "install" or ev.data.kind == "update" then
+                require("defold").download()
+            end
+        end,
+    })
+end
