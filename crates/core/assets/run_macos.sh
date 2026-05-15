d2107ffa085bfc45d3c99bd229c350218df70959
Christopher Kaster <me@atomicptr.de>
2026-05-15 09:22:10 +0200

    add support for homebrew paths on the launch script for M-series MacOS, fix #59

--- crates/core/assets/run_macos.sh
+++ crates/core/assets/run_macos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-export PATH="/usr/bin:/usr/local/bin:$PATH"
+export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:/usr/bin:/usr/local/bin:$PATH"
 "{BRIDGE_PATH}" {DEBUG_FLAG} launch-neovim {LAUNCH_PRE_ARGS} "$(realpath .)" "$1" $2 {LAUNCH_POST_ARGS}
