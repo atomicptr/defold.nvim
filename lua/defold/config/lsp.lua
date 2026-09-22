44a5254924a1eb7348f1cbb1258e9960489f257a
Christopher Kaster <me@atomicptr.de>
2026-09-22 21:57:37 +0200

    remove globals from diagnostics as they just interfere with the snippets

--- lua/defold/config/lsp.lua
+++ lua/defold/config/lsp.lua
@@ -9,17 +9,6 @@ return function()
             workspace = {
                 library = project.dependency_api_paths(),
             },
-            diagnostics = {
-                globals = {
-                    "final",
-                    "fixed_update",
-                    "init",
-                    "on_input",
-                    "on_message",
-                    "on_reload",
-                    "update",
-                },
-            },
         },
     }
 end
